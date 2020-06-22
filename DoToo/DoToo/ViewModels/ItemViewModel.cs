f1e920252bee4b1e0202aac6772471124f6b89fe
OedipusRexed <31754579+OedipusRexed@users.noreply.github.com>
2020-06-22 11:37:05 -0400

    Add project files.

---
+++ DoToo/DoToo/ViewModels/ItemViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DoToo.Models;
+using DoToo.Repositories;
+using Xamarin.Forms;
+
+namespace DoToo.ViewModels
+{
+    public class ItemViewModel : ViewModel
+    {
+        private ToDoItemRepository repository;
+
+        public ItemViewModel(ToDoItemRepository repository)
+        {
+            this.repository = repository;
+        }
+    }
+}
